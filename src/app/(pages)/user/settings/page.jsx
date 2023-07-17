ae36447d0554393080b12bfed96ddbc2a205845e
dionmar-lariosa <main.dionmar@gmail.com>
2023-07-17 17:50:31 +0700

    user profile settings validation

--- src/app/(pages)/user/settings/page.jsx
+++ src/app/(pages)/user/settings/page.jsx
@@ -1,7 +1,35 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 import Button from "@/components/Button";
 import Input from "@/components/Input";
+import InputError from "@/components/InputError";
+
+const schema = yup
+  .object({
+    name: yup.string().required(),
+    email: yup.string().required().email(),
+    phoneNumber: yup.number().required(),
+  })
+  .required();
 
 function Settings() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      name: "",
+      email: "",
+      phoneNumber: "",
+    },
+    resolver: yupResolver(schema),
+  });
+  const onSubmit = (data) => console.log(data);
+
   return (
     <div className="flex items-start justify-center">
       <div className="w-full max-w-3xl bg-white shadow-md rounded p-8 md:w-2/3 lg:w-1/2">
@@ -12,18 +40,37 @@ function Settings() {
             <hr className="border-gray-300" />
           </div>
         </div>
-        <div className="mb-4">
-          <Input type="text" label="Name" id="name" />
-        </div>
-        <div className="mb-4">
-          <Input type="email" label="Email" id="email" />
-        </div>
-        <div className="mb-4">
-          <Input type="number" label="Phone Number" id="PhoneNumber" />
-        </div>
-        <div className="flex justify-end">
-          <Button color="primary">Save Change`s</Button>
-        </div>
+        <form onSubmit={handleSubmit(onSubmit)} noValidate>
+          <div className="mb-4">
+            <Input type="text" label="Name" id="name" {...register("name")} />
+            {errors.name && <InputError message={errors.name?.message} />}
+          </div>
+          <div className="mb-4">
+            <Input
+              type="email"
+              label="Email"
+              id="email"
+              {...register("email")}
+            />
+            {errors.email && <InputError message={errors.email?.message} />}
+          </div>
+          <div className="mb-4">
+            <Input
+              type="number"
+              label="Phone Number"
+              id="phoneNumber"
+              {...register("phoneNumber")}
+            />
+            {errors.phoneNumber && (
+              <InputError message={errors.phoneNumber?.message} />
+            )}
+          </div>
+          <div className="flex justify-end">
+            <Button type="submit" color="primary">
+              Save Change`s
+            </Button>
+          </div>
+        </form>
       </div>
     </div>
   );
