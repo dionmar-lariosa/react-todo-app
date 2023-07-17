f45053ddb1e0e4cbc4dd970671e0c1c22b8f3ffa
dionmar-lariosa <main.dionmar@gmail.com>
2023-07-17 14:48:44 +0700

    welcome page

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,19 @@
+import Card from "@/components/Card";
+
 export default function Home() {
-  return <h1>Welcome page</h1>;
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <Card>
+        <h1 className="text-4xl font-bold mb-4">Welcome to Todo App</h1>
+        <p className="text-lg text-gray-600">
+          Get things done with ease. Organize your tasks efficiently.
+        </p>
+        <div className="flex justify-end mt-4">
+          <span className="text-gray-400 text-sm">
+            Made with Next.js and React.js
+          </span>
+        </div>
+      </Card>
+    </div>
+  );
 }
