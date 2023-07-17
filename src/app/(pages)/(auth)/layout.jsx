cad34419d425b794a52b5dd0341f2159fb2113d3
dionmar-lariosa <main.dionmar@gmail.com>
2023-07-17 15:49:07 +0700

    login ui and remove bg color on layout

--- src/app/(pages)/(auth)/layout.jsx
+++ src/app/(pages)/(auth)/layout.jsx
@@ -1,9 +1,7 @@
 const AuthLayout = ({ children }) => (
   <div className="flex justify-center">
     <div className="w-full max-w-md p-4">
-      <div className="bg-green-500 shadow-md rounded-md px-8 pt-6 pb-8">
-        {children}
-      </div>
+      <div className="shadow-md rounded-md px-8 pt-6 pb-8">{children}</div>
     </div>
   </div>
 );
--- src/app/(pages)/(auth)/login/page.jsx
+++ src/app/(pages)/(auth)/login/page.jsx
@@ -1,5 +1,33 @@
+import Button from "@/components/Button";
+import Input from "@/components/Input";
+
 const Login = () => {
-  return <h1>login</h1>;
+  return (
+    <form noValidate>
+      <div className="mb-4">
+        <Input type="email" label="Email" id="email" />
+      </div>
+      <div className="mb-6">
+        <Input
+          type="password"
+          label="Password"
+          id="password"
+          placeholder="********"
+        />
+      </div>
+      <div className="flex items-center justify-between">
+        <Button type="submit" color="primary">
+          Sign In
+        </Button>
+        <a
+          className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+          href="#"
+        >
+          Forgot Password?
+        </a>
+      </div>
+    </form>
+  );
 };
 
 export default Login;
