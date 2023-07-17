bb6e6fad6d4bbcbe22af4b4dd3052b660d3af4d3
dionmar-lariosa <main.dionmar@gmail.com>
2023-07-17 14:23:40 +0700

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,95 @@
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="bg-gray-900">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <Link href="/todos">
+              <span className="text-lg font-bold text-white cursor-pointer">
+                Todos
+              </span>
+            </Link>
+          </div>
+          <div className="hidden md:block">
+            <div className="ml-4 flex items-center">
+              <Link href="/user/profile">
+                <span className="ml-4 text-white cursor-pointer">profile</span>
+              </Link>
+              <Link href="/user/settings">
+                <span className="ml-4 text-white cursor-pointer">setings</span>
+              </Link>
+              <Link href="/login">
+                <span className="ml-4 text-white cursor-pointer">Login</span>
+              </Link>
+              <Link href="/register">
+                <button className="ml-4 px-4 py-2 bg-blue-500 text-white rounded">
+                  Register
+                </button>
+              </Link>
+            </div>
+          </div>
+          <div className="-mr-2 flex md:hidden">
+            <button
+              onClick={toggleMenu}
+              type="button"
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:bg-gray-800 focus:text-white transition duration-150 ease-in-out"
+              aria-label="Menu"
+            >
+              <svg
+                className="h-6 w-6"
+                stroke="currentColor"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                {isOpen ? (
+                  <path
+                    className="inline-flex"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                ) : (
+                  <path
+                    className="inline-flex"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                )}
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+      {isOpen && (
+        <div className="md:hidden">
+          <div className="px-2 pt-2 pb-3 sm:px-3">
+            <Link href="/login">
+              <span className="mt-1 block text-white">Login</span>
+            </Link>
+            <Link href="/register">
+              <button className="mt-1 px-4 py-2 block w-full text-left bg-blue-500 text-white rounded">
+                Registerp
+              </button>
+            </Link>
+          </div>
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
