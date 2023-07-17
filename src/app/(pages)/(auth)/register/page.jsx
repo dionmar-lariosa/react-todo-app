be2044c0e3e2c9b6e4c0d914f4700a8dbdb55929
dionmar-lariosa <main.dionmar@gmail.com>
2023-07-17 16:16:54 +0700

    register validation

--- src/app/(pages)/(auth)/register/page.jsx
+++ src/app/(pages)/(auth)/register/page.jsx
@@ -1,8 +1,44 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 import Button from "@/components/Button";
 import Input from "@/components/Input";
 import InputError from "@/components/InputError";
 
+const schema = yup
+  .object({
+    name: yup.string().required(),
+    email: yup.string().required().email(),
+    password: yup
+      .string()
+      .required()
+      .min(8)
+      .matches(/^\S*$/, "Password must not contain spaces"),
+    confirmPassword: yup
+      .string()
+      .required()
+      .oneOf([yup.ref("password"), null], "Passwords must match"),
+  })
+  .required();
+
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+    resolver: yupResolver(schema),
+  });
+  const onSubmit = (data) => console.log(data);
+
   return (
     <form onSubmit={handleSubmit(onSubmit)} noValidate>
       <div className="mb-4">
