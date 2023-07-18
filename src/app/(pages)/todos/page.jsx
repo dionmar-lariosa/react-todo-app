a2af3dbaaf9b55d4e6f3fea4ef6e7a5b1c9ad869
dionmar-lariosa <main.dionmar@gmail.com>
2023-07-18 13:32:20 +0700

    read/create

--- src/app/(pages)/todos/page.jsx
+++ src/app/(pages)/todos/page.jsx
@@ -9,8 +9,11 @@ import {
   Maximize2,
 } from "react-feather";
 import Card from "@/components/Card";
+import { useEffect, useState } from "react";
 
 const Todos = () => {
+  const [todos, setTodos] = useState([]);
+
   const {
     register,
     handleSubmit,
@@ -22,8 +25,13 @@ const Todos = () => {
   });
 
   const onSubmit = (data) => {
-    console.log(data);
+    const prepData = { id: Date.now(), todo: data.todo };
+    setTodos([...todos, prepData]);
   };
+
+  useEffect(() => {
+    console.log(todos);
+  }, [todos]);
 
   return (
     <>
@@ -34,51 +42,53 @@ const Todos = () => {
         <form onSubmit={handleSubmit(onSubmit)}>
           <textarea
             className="w-full px-3 py-2 border border-gray-300  focus:outline-none focus:border-blue-500 rounded-md"
-            name=""
-            id=""
+            id="todo"
             cols="1"
             rows="1"
+            {...register("todo")}
           ></textarea>
           <button
             type="submit"
-            class="w-full px-4 py-2 bg-blue-500 text-white rounded-md"
+            className="w-full px-4 py-2 bg-blue-500 text-white rounded-md"
           >
             Submit
           </button>
         </form>
       </div>
-      <div className="grid gap-5 md:grid-cols-3">
-        <Card>
-          <div className="p-4 flex flex-col">
-            <div className="flex items-center space-x-4 mb-2">
-              <Edit
-                size={24}
-                className="cursor-pointer hover:text-yellow-500"
-                onClick={() => handleEditClick(index)}
-              />
-              <Trash2
-                size={24}
-                className="cursor-pointer hover:text-red-500"
-                onClick={() => console.log("Trash2")}
-              />
-              <Maximize2
-                size={24}
-                className="cursor-pointer hover:text-gray-500"
-                onClick={() => console.log("Maximize2")}
-              />
-              <CheckSquare
-                size={24}
-                className="cursor-pointer hover:text-green-500"
-                onClick={() => console.log("CheckSquare")}
-              />
-            </div>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor,
-              tempora.
-            </p>
-          </div>
-        </Card>
-      </div>
+      {todos && todos.length > 0 && (
+        <div className="grid gap-5 md:grid-cols-3">
+          {todos.map((todo) => {
+            return (
+              <Card key={todo.id}>
+                <div className="p-4 flex flex-col">
+                  <div className="flex items-center space-x-4 mb-2">
+                    <Edit
+                      size={24}
+                      className="cursor-pointer hover:text-yellow-500"
+                    />
+                    <Trash2
+                      size={24}
+                      className="cursor-pointer hover:text-red-500"
+                      onClick={() => console.log("Trash2")}
+                    />
+                    <Maximize2
+                      size={24}
+                      className="cursor-pointer hover:text-gray-500"
+                      onClick={() => console.log("Maximize2")}
+                    />
+                    <CheckSquare
+                      size={24}
+                      className="cursor-pointer hover:text-green-500"
+                      onClick={() => console.log("CheckSquare")}
+                    />
+                  </div>
+                  <p>{todo.todo}</p>
+                </div>
+              </Card>
+            );
+          })}
+        </div>
+      )}
     </>
   );
 };
